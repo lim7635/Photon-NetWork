34563741e89c6a02f15fb817b96ffef5192169dd
lim7635 <ju7635@naver.com>
2024-07-17 21:45:30 +0900

    2024.07.17 로비 접속 시스템 추가

---
+++ Photon Server Game/Assets/Scripts/ConnectServer.cs
@@ -0,0 +1,47 @@
+using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+using TMPro;
+
+public class ConnectServer : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Canvas canvasRoom;
+    [SerializeField] Canvas canvasLobby;
+    [SerializeField] TMP_Dropdown server;
+
+    private void Awake()
+    {
+        server.options[0].text = "Asia";
+        server.options[1].text = "Europe";
+        server.options[2].text = "America";
+
+        if(PhotonNetwork.IsConnected)
+        {
+            canvasLobby.gameObject.SetActive(false);
+        }
+    }
+
+    public void SelectServer()
+    {
+        // 서버 접속
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        canvasRoom.sortingOrder = 1;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        // JoinLobby : 특정 로비를 생성하여 진입하는 방법
+        PhotonNetwork.JoinLobby
+        (
+            new TypedLobby
+            (
+                server.options[server.value].text,
+                LobbyType.Default
+            )
+        );
+    }
+}
