0df1694b9afbdbf7e501547577a9de9e0fdc2d7f
lim7635 <ju7635@naver.com>
2024-07-22 21:45:30 +0900

    2024.07.22 포톤 객체 생성 시스템

--- Photon Server Game/Assets/Scripts/Controller.cs
+++ Photon Server Game/Assets/Scripts/Controller.cs
@@ -1,13 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using PlayFab.EventsModels;
 
-public class Controller : MonoBehaviour
+public class Controller : MonoBehaviourPun
 {
+    [SerializeField] float mouseX;
+
+    [SerializeField] float speed;
+
+    [SerializeField] Vector3 direction;
+
+    [SerializeField] Camera temporaryCamera;
 
     void Start()
     {
-        
+        // 현재 플레이어가 본인일 경우
+        if (photonView.IsMine)
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+        else
+        {
+            temporaryCamera.enabled = false;
+            GetComponentInChildren<AudioListener>().enabled = false;
+        }
     }
 
     void Update()
